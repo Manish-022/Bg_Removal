7c2bb3187432727d645f346fcb5bba46ef196ce4
Manish Kumar <mk9784129@gmail.com>
2025-06-21 15:30:55 +0530

    Commit6

--- server/configs/mongodb.js
+++ server/configs/mongodb.js
@@ -1,11 +1,51 @@
+// import mongoose from "mongoose";
+
+// const connectDB = async () => {
+//   mongoose.connection.on("connected", () => {
+//     console.log("Database Connected");
+//   });
+//   await mongoose.connect(`${process.env.MONGODB_URI}/BgRemoval`);
+// };
+
+// export default connectDB;
+
+
+//
+
 import mongoose from "mongoose";
 
+// Serverless connection caching
+let cached = global.mongoose;
+
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
+
 const connectDB = async () => {
-  mongoose.connection.on("connected", () => {
-    console.log("Database Connected");
-  });
-  await mongoose.connect(`${process.env.MONGODB_URI}/BgRemoval`);
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      serverSelectionTimeoutMS: 5000,
+      socketTimeoutMS: 30000,
+    };
+
+    cached.promise = mongoose
+      .connect(`${process.env.MONGODB_URI}/BgRemoval`, opts)
+      .then((mongoose) => {
+        console.log("Database Connected");
+        return mongoose;
+      })
+      .catch((err) => {
+        console.error("Database Connection Error:", err);
+        throw err;
+      });
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 };
 
 export default connectDB;
-
