160703f55f7a46925d08aef9836d590fb7ab9967
Manish Kumar <mk9784129@gmail.com>
2025-06-27 17:20:55 +0530

    Commit10

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -1,12 +1,72 @@
+// import { Webhook } from "svix";
+// import userModel from "../models/userModel.js";
+
+// //API Controller function to manage clerk User with databse
+// // http://localhost:4000/api/user/webhooks
+
+// const clerkWebhooks = async (req, res) => {
+//   try {
+//     //create a Svix instance with clerk webhook secret
+//     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
+
+//     await whook.verify(JSON.stringify(req.body), {
+//       "svix-id": req.headers["svix-id"],
+//       "svix-timestamp": req.headers["svix-timestamp"],
+//       "svix-signature": req.headers["svix-signature"],
+//     });
+
+//     const { data, type } = req.body;
+
+//     switch (type) {
+//       case "user.created": {
+//         const userData = {
+//           clerkId: data.id,
+//           email: data.email_addresses[0].email_address,
+//           firstName: data.first_name,
+//           lastName: data.last_name,
+//           photo: data.image_url,
+//         };
+
+//         await userModel.create(userData);
+//         res.json({});
+//         break;
+//       }
+
+//       case "user.updated": {
+//         const userData = {
+//           email: data.email_addresses[0].email_address,
+//           firstName: data.first_name,
+//           lastName: data.last_name,
+//           photo: data.image_url,
+//         };
+//         await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
+//         res.json({});
+//         break;
+//       }
+
+//       case "user.deleted": {
+//         await userModel.findOneAndDelete({ clerkId: data.id });
+//         res.json({});
+//         break;
+//       }
+
+//       default:
+//         break;
+//     }
+//   } catch (error) {
+//     console.log(error.message);
+//     res.json({ success: false, message: error });
+//   }
+// };
+
+// export {clerkWebhooks}
+
+// new code
 import { Webhook } from "svix";
 import userModel from "../models/userModel.js";
 
-//API Controller function to manage clerk User with databse
-// http://localhost:4000/api/user/webhooks
-
 const clerkWebhooks = async (req, res) => {
   try {
-    //create a Svix instance with clerk webhook secret
     const whook = new Webhook(process.env.CLERK_WEBHOOK_SECRET);
 
     await whook.verify(JSON.stringify(req.body), {
@@ -21,42 +81,50 @@ const clerkWebhooks = async (req, res) => {
       case "user.created": {
         const userData = {
           clerkId: data.id,
-          email: data.email_addresses[0].email_address,
+          email: data.email_addresses?.[0]?.email_address || "",
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
         };
 
-        await userModel.create(userData);
-        res.json({});
+        await userModel.findOneAndUpdate({ clerkId: data.id }, userData, {
+          upsert: true,
+          new: true,
+        });
+
         break;
       }
 
       case "user.updated": {
         const userData = {
-          email: data.email_addresses[0].email_address,
+          email: data.email_addresses?.[0]?.email_address || "",
           firstName: data.first_name,
           lastName: data.last_name,
           photo: data.image_url,
         };
-        await userModel.findOneAndUpdate({ clerkId: data.id }, userData);
-        res.json({});
+
+        await userModel.findOneAndUpdate({ clerkId: data.id }, userData, {
+          new: true,
+        });
+
         break;
       }
 
       case "user.deleted": {
         await userModel.findOneAndDelete({ clerkId: data.id });
-        res.json({});
         break;
       }
 
       default:
+        console.log(`Unhandled webhook event type: ${type}`);
         break;
     }
+
+    res.json({ success: true });
   } catch (error) {
-    console.log(error.message);
-    res.json({ success: false, message: error });
+    console.log(error);
+    res.status(500).json({ success: false, message: error.message });
   }
 };
 
-export {clerkWebhooks}
+export { clerkWebhooks };
